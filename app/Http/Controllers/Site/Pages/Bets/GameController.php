473f3ac741c2a7aa0f8c8c898be01f2c48a86737
alodener <paulodenerg@gmail.com>
2021-10-26 16:51:42 -0300

    Correção do BUG de Voltar para Finalizar Aposta

--- app/Http/Controllers/Site/Pages/Bets/GameController.php
+++ app/Http/Controllers/Site/Pages/Bets/GameController.php
@@ -29,13 +29,13 @@ class GameController extends Controller
         try {
             $date = Carbon::now();
                 
-            /*if ( $date->hour >=20 || $date->hour < 00) {
+            if ( $date->hour >=20 || $date->hour < 00) {
             $bet = null;
             $typeGames = TypeGame::get();
             session()->flash('error', 'Apostas Encerradas!');
             return view('site.bets.games.bets.index', compact('user', 'bet', 'typeGames'));
        
-        }*/
+        }
             $bet = new Bet();
             $bet->user_id = $user->id;
             $bet->save();
@@ -50,18 +50,21 @@ class GameController extends Controller
 
     public function gameCreate($user, Bet $bet, TypeGame $typeGame)
     {
+        
         return view('site.bets.games.bets.create', compact('bet', 'typeGame'));
     }
 
     public function store(Bet $bet, $typeGame, $selectedNumbers, $valor, $premio,$valueid)
     {
-        
-          /*   $date = Carbon::now();
-                
-            if ( $date->hour >=20 || $date->hour < 06) {
+
+        if ($bet->status == false) {
+          throw new \Exception('Aposta Já finalizada');
+        }
+            $date = Carbon::now();   
+            if ( $date->hour >=20 || $date->hour < 00) {
             throw new \Exception('Apostas encerradas');
        
-        }*/
+        }
         sort($selectedNumbers, SORT_NUMERIC);
         $balance = Balance::calculationByHash($valor, $bet->user);
 
@@ -119,21 +122,34 @@ class GameController extends Controller
         $game->commission_value = $commissionCalculation;
         $game->save();
 
+        $bet->botao_finalizar = 0;
+        $bet->save();
+
         return $game;
+    
 
     }
 
     public function betUpdate(User $user, Bet $bet)
     {
+
+        
         try {
+            if($bet->botao_finalizar = 3){
+                return view('site.bets.games.bets.bet-create', compact('bet'));
+            }else{
+
+           
+            $bet->botao_finalizar = 3;
             $bet->status = false;
             $bet->save();
-
             return view('site.bets.games.bets.bet-create', compact('bet'));
+             }
         } catch (Exception $exception) {
             session()->flash('error', config('app.env') != 'production' ? $exception->getMessage() : 'Ocorreu um erro no processo!');
             return redirect()->route('games.bet', ['user' => $user->id, 'bet' => $bet->id]);
         }
+    
     }
 
     public function setClient(Bet $bet, $clientId)
