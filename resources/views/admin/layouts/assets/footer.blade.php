211da16d7e75e8d8983a72efc28b37fe0ce7064f
alodener <paulodenerg@gmail.com>
2022-01-15 00:37:21 -0300

    Adequação do footer

--- resources/views/admin/layouts/assets/footer.blade.php
+++ resources/views/admin/layouts/assets/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="float-right d-none d-sm-block">
         <b>Versão</b> 1.0.0
     </div>
-    <strong>Copyright &copy; 2020-2020 Lotogiro.</strong>
+    <strong>Copyright &copy; 2020-2022 SuperLotogiro.</strong>
     Todos
     direitos
     reservados.
