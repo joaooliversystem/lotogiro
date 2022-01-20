d9a853c1eddbe58af24b9975a6ab69aa504ef41e
Kelver <kelver_kmv@hotmail.com>
2022-01-20 19:18:11 -0300

    Tela de Listagem de solicitações de saque.

---
+++ app/Http/Livewire/Pages/Dashboards/Wallet/Withdraw/AdminList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Pages\Dashboards\Wallet\Withdraw;
+
+use Livewire\Component;
+
+class AdminList extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.dashboards.wallet.withdraw.admin-list');
+    }
+}
