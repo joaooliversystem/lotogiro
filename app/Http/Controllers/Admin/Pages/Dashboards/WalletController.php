26da2d618f87869ae48fbfef14ed8d42cdc97756
Kelver <kelver_kmv@hotmail.com>
2022-02-07 23:22:26 -0300

    Correção de comissionamento.

--- app/Http/Controllers/Admin/Pages/Dashboards/WalletController.php
+++ app/Http/Controllers/Admin/Pages/Dashboards/WalletController.php
@@ -74,19 +74,25 @@ class WalletController extends Controller
                 $newRechargeOrder->status = $typeStatus[$request->status];
                 $newRechargeOrder->push();
 
+                $commission = 0;
+                $totalRecharge = $newRechargeOrder->value;
+                $msgCommission = "";
+                if($user->commission > 0){
+                    $commission = $newRechargeOrder->value * ($user->commission/100);
+                    $totalRecharge = $newRechargeOrder->value + $commission;
+                    $msgCommission = "Mais {$user->commission}% de comissão.";
+                }
+
                 if($typeStatus[$request->status] === 1){
                     TransactBalance::create([
                         'user_id_sender' => 1,
                         'user_id' => $user->id,
-                        'value' => $newRechargeOrder->value,
+                        'value' => $totalRecharge,
                         'old_value' => $user->balance,
-                        'type' => 'Recarga efetuada por meio da plataforma.'
+                        'type' => "Recarga efetuada por meio da plataforma. {$msgCommission}"
                     ]);
-                    if($user->comission > 0){
-                        $newRechargeOrder->value *= ($user->comission / 100);
-                    }
 
-                    $user->balance += $newRechargeOrder->value;
+                    $user->balance += $newRechargeOrder->value + $commission;
                     $user->save();
                 }
 
