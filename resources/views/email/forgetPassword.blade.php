56342596fd890655fe077eca380c09a8d39bd984
KlecioHenrique <kleciohenrique18@gmail.com>
2022-03-10 17:21:05 -0300

    recuperar senha

---
+++ resources/views/email/forgetPassword.blade.php
@@ -0,0 +1,13 @@
+<center>
+    <img src="https://superjogo.loteriabr.com/admin/images/painel/Trevo.png" alt="" width=150 height=150>
+
+    <h1>Reset de Senha</h1>
+</center>
+
+<pre>
+    Olá, você solicitou o reset de senha, segue o link abaixo para realizar o procedimento:
+    
+    <a href="{{ route('reset.password.get', $token) }}">Recuperar Senha</a>
+
+    Atenciosamente,
+    Superlotogiro</pre>
