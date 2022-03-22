a150bc6a7d25c001a5934a42551fcfee5c14d5db
alodener <paulodenerg@gmail.com>
2022-03-22 15:00:56 -0300

    rodape

--- resources/views/admin/layouts/assets/footer.blade.php
+++ resources/views/admin/layouts/assets/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="float-right d-none d-sm-block">
         <b>Versão</b> 3.1.0
     </div>
-    <strong>Copyright &copy; 2020-2022 SuperLotogiro.</strong>
+    <strong>Copyright &copy; 2022 Giro da Sorte.</strong>
     Todos
     direitos
     reservados.
