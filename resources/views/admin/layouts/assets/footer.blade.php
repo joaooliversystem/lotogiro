3345de2a4c71394ca20d272c6d71493c20686f7d
joaooliversystem <74998239+joaooliversystem@users.noreply.github.com>
2022-04-19 15:56:29 -0300

    delete

--- resources/views/admin/layouts/assets/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-        <b>Versão</b> 1.0.0
-    </div>
-    <strong>Copyright &copy; 2020-2020 Lotogiro.</strong>
-    Todos
-    direitos
-    reservados.
-</footer>
